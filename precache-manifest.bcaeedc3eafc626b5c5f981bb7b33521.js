abf19caf0bc5b3faba4d7c727d683a62a33ff0ca
Nguedia Adele <nguediaadele@yahoo.fr>
2019-09-24 12:34:36 +0100

    Updates

---
+++ precache-manifest.bcaeedc3eafc626b5c5f981bb7b33521.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5c9a9bfe8182c1c3da5b093ade4c31",
+    "url": "/PH-clone-react/index.html"
+  },
+  {
+    "revision": "99cade3c4afdac10aa0a",
+    "url": "/PH-clone-react/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "177b17b5bc7bbd6bf2ef",
+    "url": "/PH-clone-react/static/css/main.3e5d2bdc.chunk.css"
+  },
+  {
+    "revision": "99cade3c4afdac10aa0a",
+    "url": "/PH-clone-react/static/js/2.e140d20e.chunk.js"
+  },
+  {
+    "revision": "177b17b5bc7bbd6bf2ef",
+    "url": "/PH-clone-react/static/js/main.e682cbf0.chunk.js"
+  },
+  {
+    "revision": "e53a08f275455300723a",
+    "url": "/PH-clone-react/static/js/runtime~main.10e8da89.js"
+  },
+  {
+    "revision": "234c9290d0bf1be6410a5856daa3685a",
+    "url": "/PH-clone-react/static/media/logo.234c9290.png"
+  }
+]);
